456b5a5dd939bfcae1bfd7bcacf5552ece34d13a
Bruno Ferraz <brunoferraz.pro@gmail.com>
2020-04-19 23:18:54 -0300

    [2]OK- FINISHED get coordinates

--- clearmunicipios.py
+++ clearmunicipios.py
@@ -1,34 +1,57 @@
+from pykml.factory import KML_ElementMaker as KML
+from pykml.factory import ATOM_ElementMaker as ATOM
+from lxml import etree
 from pykml import parser
-
-with open("toy.kml", "r", encoding="utf8") as f:
-    doc = parser.parse(f)
-
-it = doc.iter()
-print(it[0])
-
-# PESQUISAR SOBRE ITERADORES
-
-# from pykml import parser
-# from os import path
-# from lxml import etree
-
-# # f = open("BR_Localidades_2010_v1.kml", "r")
-# # f = open("novo.txt", "r")
-# kml_file = path.join('BR_Localidades_2010_v1.kml')
-
-# doc = etree.ElementTree
-# # with open("BR_Localidades_2010_v1.kml", "r", encoding="utf8") as f:
-# with open(kml_file, "r", encoding="utf8") as f:
-#     doc = parser.parse(f).getroot()
+from os import path
 
 
-# print(doc.Document.Schema)
-# print("dfsdfds")
+# doc = etree
+# with open("toy.kml", "r", encoding="utf8") as f:
+#     doc = parser.parse(f)
 
-# # it = doc.iter()
-# # print(doc[0])
+# root 
+# with open("toy.kml", "r", encoding="utf8") as f:
 
-# # print(it)
-# # for i in it:
-#     # print(i)
-# # print(doc.iter())
+# with open("toy.kml", "r") as fobj:
+#         xml = fobj.read()
+# root = parser.fromstring(xml)
+# print(root)
+
+# data = open('toy.kml', 'rb')
+# xslt_content = data.read()
+# root = etree.XML(xslt_content)
+# print(root.tag)
+
+    
+# with open("toy.kml", "r") as fobj:
+#         xml = fobj.read()
+# root = etree.XML(xml.encode())
+
+# for i in root:
+#     print(i)
+#CD_GEOCODMU
+
+from os import path
+from xml.etree.ElementTree import Element, SubElement, ElementTree
+
+# kml_file = path.join('toy.kml')
+kml_file = path.join('BR_Localidades_2010_v1.kml')
+
+doc = etree
+with open(kml_file, encoding="utf8") as f:
+    doc = parser.parse(f).getroot()
+
+file1 = open("coordinates.csv","a", encoding="utf8") 
+
+
+csv_str = "IBGE_CODE, NOME, LONGITUDE, LATITUDE, ALTURA \n"
+for i in doc.Document.Folder.Placemark:
+    nome = i.name
+    ibgeCode = i.ExtendedData.SchemaData.SimpleData[9]
+    coordenadas = i.Point.coordinates
+    csv_str += str(ibgeCode) + ","
+    csv_str += nome + ","
+    csv_str += coordenadas + "\n"  
+
+file1.write(csv_str) 
+file1.close() 
